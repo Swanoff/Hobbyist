5d24fabbed234af237da2ac9fc3e2fa5614f8a98
Ram narayanan <[ramnaryanan51@gmail.com]>
2018-04-03 19:34:30 +0530

    Added unique email and username identification during registration

--- Hobbyist/hobby_app/forms.py
+++ Hobbyist/hobby_app/forms.py
@@ -9,3 +9,14 @@ class UserForm(forms.ModelForm):
     class Meta():
         model = User
         fields = ('username','email','password')
+        #unique_together = ('username',)
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        username = self.cleaned_data.get('username')
+
+        if email and User.objects.filter(email=email).exclude(username=username).exists():
+            raise forms.ValidationError(u'Email must be unique')
+
+
+        return email
